82175647525f582542e4be6d07ae03aacb5ca55e
salauddin-1997 <56165472+salauddin-1997@users.noreply.github.com>
2019-12-29 20:22:29 +0600

    Add files via upload

--- js/custom.js
+++ js/custom.js
@@ -1,11 +1,44 @@
 (function ($) {
     'use strict'
 
+     // AOS section
+     AOS.init();
+
+
+
+
+    var owl = $('.owl-carousel');
+owl.owlCarousel({
+    items:4,
+    loop:true,
+    margin:10,
+    autoplay:true,
+    autoplayTimeout:2000,
+    autoplayHoverPause:true,
+    responsiveClass:true,
+    responsive:{
+        0:{
+            items:2,
+            nav:true
+        },
+        600:{
+            items:2,
+            nav:false
+        },
+        1000:{
+            items:4,
+            nav:true,
+            loop:true
+        }
+    }
+});
     
     lightbox.option({
       'resizeDuration': 200,
       'wrapAround': true
     });
+
+
 
 
 
