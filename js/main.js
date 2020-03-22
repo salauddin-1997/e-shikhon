b1c3f9b2a916f037c3dc7f21a8f22aac3f10f9f3
salauddin-1997 <56165472+salauddin-1997@users.noreply.github.com>
2020-03-22 15:27:07 +0600

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,33 @@
+(function($) {
+
+	"use strict";
+
+	var fullHeight = function() {
+
+		$('.js-fullheight').css('height', $(window).height());
+		$(window).resize(function(){
+			$('.js-fullheight').css('height', $(window).height());
+		});
+
+	};
+	fullHeight();
+
+	$('#sidebarCollapse').on('click', function () {
+      $('#sidebar').toggleClass('active');
+  });
+
+  var close = document.getElementsByClassName("closebtn");
+  var i;
+  
+  for (i = 0; i < close.length; i++) {
+    close[i].onclick = function(){
+      var div = this.parentElement;
+      div.style.opacity = "0";
+      setTimeout(function(){ div.style.display = "none"; }, 600);
+    }
+  }
+
+
+
+
+})(jQuery);
